dad3c015da184ce27245f92881eee67a5800ac8f
ArtMejia <arteniomejia.student@careerdevs.com>
2022-06-21 19:34:06 -0400

    JavaBasics Challenge Set 1: 1/3 - COMPLETE

---
+++ src/com/mejia/challenge1/Main.java
@@ -0,0 +1,12 @@
+package com.mejia.challenge1;
+
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Artenio";
+        String lastName = "Mejia";
+
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+
+}
